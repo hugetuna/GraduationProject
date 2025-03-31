42f7ba155b234a543170a51ef2b9c5d3544480d5
Nomura369 <jenny369314@gmail.com>
2025-04-01 01:28:13 +0800

    Training UI updates

--- Assets/Scripts/Schedule/TrainingUIHandler.cs
+++ Assets/Scripts/Schedule/TrainingUIHandler.cs
@@ -1,12 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class TrainingUIHandler : MonoBehaviour
 {
     public GameObject traningUI;
-    // public PlayerControlMainWorld playerController; // 玩家控制腳本
-    // public List<PlayerControlMainWorld> teamMembers; // 隊伍成員列表
+    private GameObject traningUIInstance = null; // 記錄被生成的訓練 UI
 
     void Start()
     {
@@ -15,7 +15,14 @@ public class TrainingUIHandler : MonoBehaviour
 
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0)) // 檢查滑鼠左鍵是否被按下
+        {
+            if (!IsCursorClickUIObject() && traningUIInstance != null) // 點擊非 UI 區域時關閉 UI
+            {
+                Debug.Log("關閉訓練 UI");
+                traningUIInstance.SetActive(false); 
+            }
+        }
     }
 
     void OnDestroy()
@@ -23,14 +30,28 @@ public class TrainingUIHandler : MonoBehaviour
         DoorInteraction.onDoorInteracted += ShowTrainingUI; // 取消訂閱事件
     }
 
-    void ShowTrainingUI(){
+    private void ShowTrainingUI(){
         Debug.Log("開啟訓練 UI");
-        Instantiate(traningUI); // （每日結算時再進行銷毀）
-        // playerController.enabled = false; // 禁用玩家控制
-        
-        // // 取得 TeamManager 腳本
-        // TeamManager teamManager = FindObjectOfType<TeamManager>();
-        // // 複製 TeamManager 腳本的成員列表
-        // teamMembers = new List<PlayerControlMainWorld>(teamManager.teamMembers);
+        if(traningUIInstance == null){
+            traningUIInstance = Instantiate(traningUI); // （每日結算時再進行銷毀）
+        }
+        else {
+            traningUIInstance.SetActive(true); // 如果 UI 已經存在，則顯示它
+        }
+        // Time.timeScale = 0; // 禁用玩家控制（以暫停遊戲的方式）
+    }
+
+    private bool IsCursorClickUIObject()
+    {
+        // 根據當前操作，設定滑鼠或觸控位置
+        PointerEventData eventData = new PointerEventData(EventSystem.current);
+        eventData.position = Input.mousePosition;
+
+        // RaycastAll 會從 eventData 中的滑鼠位置發射一條射線，檢測所有碰撞的 UI 元素
+        // 符合條件的 UI 元素會被加到 raycastResults 清單中
+        var raycastResults = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventData, raycastResults);
+
+        return raycastResults.Count > 0;
     }
 }
