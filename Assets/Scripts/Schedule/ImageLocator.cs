ba08a42a1b7d69e8aeb081aae719d8b5b919b409
Nomura369 <jenny369314@gmail.com>
2025-04-19 19:14:19 +0800

    Character UI Images

---
+++ Assets/Scripts/Schedule/ImageLocator.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageLocator : MonoBehaviour
+{
+    public Image[] characterImages;
+
+    // Start is called before the first frame update
+    // void Start()
+    // {
+        
+    // }
+
+    // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+}
--- Assets/Scripts/Schedule/TrainingUIHandler.cs
+++ Assets/Scripts/Schedule/TrainingUIHandler.cs
@@ -2,31 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class TrainingUIHandler : MonoBehaviour
 {
-    public GameObject traningUI;
-    public GameObject teamManager; // 預計使用 TeamManager 取得當前隊伍成員
-    private GameObject traningUIInstance = null; // 記錄被生成的訓練 UI
+    public GameObject trainingUI;
+    private GameObject trainingUIInstance = null; // 記錄被生成的訓練 UI
+
+    public TeamManager teamManager; // 透過 TeamManager 物件取得當前隊伍成員
+    [SerializeField] private List<PlayerControlMainWorld> teamMembers = new(); // 記錄取得的隊伍成員
+    [SerializeField] private Image[] characterImages; // 顯示在 UI 上的（角色）圖片
+    [SerializeField] private Sprite[] characterSprites; // 角色圖片來源
+    [SerializeField] private Dictionary<string, Sprite> characterSpriteDict; // 角色名稱與圖片來源的對照表
+
+
 
     void Start()
     {
         DoorInteraction.OnDoorInteracted += ShowTrainingUI; // 訂閱並監聽事件
+
+        characterSpriteDict = new() { // 建立對照表內容
+            { "Kuma", characterSprites[0] },
+            { "Karo", characterSprites[1] },
+            { "Sirius", characterSprites[2] },
+        };
     }
 
     void Update()
     {
         if (Input.GetMouseButtonDown(0)) // 檢查滑鼠左鍵是否被按下
         {
-            if (!IsCursorClickUIObject() && traningUIInstance != null) // 點擊非 UI 區域時關閉 UI
+            if (!IsCursorClickUIObject() && trainingUIInstance != null) // 點擊非 UI 區域時關閉 UI
             {
                 Debug.Log("關閉訓練 UI");
-                traningUIInstance.SetActive(false); 
+                trainingUIInstance.SetActive(false);
             }
-        }
-
-        if(traningUIInstance != null && traningUIInstance.activeSelf){
-            // 若訓練 UI 被開啟，就按照目前隊伍的狀態來更新 UI
         }
     }
 
@@ -35,15 +45,34 @@ public class TrainingUIHandler : MonoBehaviour
         DoorInteraction.OnDoorInteracted += ShowTrainingUI; // 取消訂閱事件
     }
 
-    private void ShowTrainingUI(){
+    private void ShowTrainingUI()
+    {
         Debug.Log("開啟訓練 UI");
-        if(traningUIInstance == null){
-            traningUIInstance = Instantiate(traningUI); // （每日結算時再進行銷毀）
+        if (trainingUIInstance == null)
+        {
+            trainingUIInstance = Instantiate(trainingUI); // （每日結算時再進行銷毀）
         }
-        else {
-            traningUIInstance.SetActive(true); // 如果 UI 已經存在，則顯示它
+        else
+        {
+            trainingUIInstance.SetActive(true); // 如果 UI 已經存在，則顯示它
         }
-        // Time.timeScale = 0; // 禁用玩家控制（以暫停遊戲的方式）
+
+
+        // 取得 UI 圖片的插槽位置
+        ImageLocator imageLocator = trainingUIInstance.GetComponent<ImageLocator>();
+        characterImages = imageLocator.characterImages;
+
+        // 根據目前隊伍成員決定 UI 的初始樣貌（但因為 TeamManager 的清單是固定的，所以看起來都一樣）
+        teamMembers = teamManager.teamMembers;
+        for (int i = 0; i < teamMembers.Count && i < characterImages.Length; i++) // 確保不會超出陣列範圍
+        {
+            string memberName = teamMembers[i].name; // 取得隊伍成員名稱
+            memberName = memberName[10..]; // 去除前綴（只剩名字）
+            if (characterSpriteDict.ContainsKey(memberName)) // 檢查對照表中是否有該名稱
+            {
+                characterImages[i].sprite = characterSpriteDict[memberName]; // 指派圖片來源
+            }
+        }
     }
 
     private bool IsCursorClickUIObject()
