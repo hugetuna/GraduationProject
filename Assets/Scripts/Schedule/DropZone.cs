ef9a2f78732dbc7a18cfe633fdd0328f52b277cd
Nomura369 <jenny369314@gmail.com>
2025-04-22 23:24:31 +0800

    Drag and Drop

---
+++ Assets/Scripts/Schedule/DropZone.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropZone : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public static DropZone currentDragZone;
+
+    public RectTransform myPos;
+
+    private void Awake()
+    {
+        myPos = GetComponent<RectTransform>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData) // 當滑鼠將角色拖曳過來
+    {
+        currentDragZone = this;
+    }
+
+    public void OnPointerExit(PointerEventData eventData) // 當滑鼠將角色拖曳離開
+    {
+        if (currentDragZone == this) currentDragZone = null;
+    }
+}
