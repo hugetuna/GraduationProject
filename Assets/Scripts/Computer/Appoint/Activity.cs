94b58eb5daffb77d4e7f83e14c39945255769113
Nomura369 <jenny369314@gmail.com>
2026-03-05 20:17:17 +0800

    Working on Activity UI - 1

--- Assets/Scripts/Computer/Appoint/Activity.cs
+++ Assets/Scripts/Computer/Appoint/Activity.cs
@@ -6,7 +6,8 @@ public class Activity : ScriptableObject
     public string activityName; // 活動名稱
     public string description; // 活動描述
     public Sprite activityImage; // 活動圖片
-    public int fee; // 活動價格
     public string date; // 活動日期（格式：YYYY.MM.DD）
     public int vigourCost; // 活動耗體
+    public int fee; // 活動費用
+    public int MoneyGain; // 活動的金錢收益
 }
