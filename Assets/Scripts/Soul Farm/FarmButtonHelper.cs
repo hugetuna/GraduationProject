c0fa8b10cee292c577a45340efdbb6e76f930a6a
hugetuna <q22589761@gmail.com>
2026-05-20 15:23:37 +0800

    farm ui update

--- Assets/Scripts/Soul Farm/FarmButtonHelper.cs
+++ Assets/Scripts/Soul Farm/FarmButtonHelper.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
@@ -5,6 +6,8 @@ using UnityEngine.UI;
 public class FarmButtonHelper : MonoBehaviour, IPointerEnterHandler
 {
     private Button _button;
+    public TMPro.TextMeshProUGUI ButtonText;
+    public TMPro.TextMeshProUGUI countingText;
 
     void Awake() => _button = GetComponent<Button>();
 
@@ -14,5 +17,15 @@ public class FarmButtonHelper : MonoBehaviour, IPointerEnterHandler
         // 強制將 EventSystem 的選取對象改為此按鈕
         // 這樣按下鍵盤空白鍵時，觸發的就是目前的按鈕
         EventSystem.current.SetSelectedGameObject(this.gameObject);
+    }
+    public void OnSelected()
+    {
+        ButtonText.color=new Color32(249, 180, 195, 255);
+        if (countingText != null) countingText.color = new Color32(249, 180, 195, 255);
+    }
+    public void OnDeselected()
+    {
+        ButtonText.color = Color.white;
+        if (countingText != null) countingText.color = Color.white;
     }
 }
