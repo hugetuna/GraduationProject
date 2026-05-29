fca821c586b3482d675059deb65be2c721a67c5a
hugetuna <q22589761@gmail.com>
2026-05-29 14:41:32 +0800

    ClothRoomUI

--- Assets/Scripts/Tools/SomeInteractableThings/ClothChange.cs
+++ Assets/Scripts/Tools/SomeInteractableThings/ClothChange.cs
@@ -8,21 +8,9 @@ public class ClothChange : MonoBehaviour,IInteractable
     public string InteractionKey => "ClothChange"; // 這個字串用來指定動畫 key
     public TeamManager teamManager;
     public Dictionary<string, SpriteResolver> resolvers = new Dictionary<string, SpriteResolver>();
-    public bool isUsed = false;
+    public Canvas canvas_ClothChange;
     public void Interact(int tool)
     {
-        teamManager = FindAnyObjectByType<TeamManager>();
-        PlayerControlMainWorld leader = teamManager.teamMembers[teamManager.currentLeaderIndex];
-        IdolInstance idol = leader.GetComponent<IdolInstance>();
-        if(!isUsed)
-        {
-            idol.ChangeCloth(1);
-            isUsed=!isUsed;
-        }
-        else
-        {
-            idol.ChangeCloth(0);
-            isUsed=!isUsed;
-        }
+        canvas_ClothChange.gameObject.SetActive(true);
     }
 }
